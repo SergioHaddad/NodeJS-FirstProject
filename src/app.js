d93433f993befa51a5dfce76704be610a5fd35b3
Sergio Haddad <haddads@bos.edu>
2021-08-30 12:36:53 +0300

    New Dir

--- src/app.js
+++ src/app.js
@@ -1,98 +1,102 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-const Dsc = require('./utils/PocketTest')
-const { RSA_NO_PADDING } = require('constants')
-// console.log(__dirname)
-// console.log(path.join(__dirname, '../public'))
 
-
-const port = process.env.PORT || 3000
-/////////Defines paths for Express config 
 const app = express()
-const publicDrectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const PartialPath = path.join(__dirname,'../templates/partials')
-//setup handlebars engine and views location
-app.set('view engine', '.hbs')
-app.set('views',viewsPath)
-hbs.registerPartials(PartialPath)
+const port = process.env.PORT || 3000
 
-//setup static directory to serve 
-app.use('/public',express.static(publicDrectoryPath))
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-// const AboutPath = path.join(__dirname, '../public/about.html')
-// app.use(express.static(AboutPath))
-// app.get('/about', (req,res) => {
-//     res.render('about')  
-// })
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-// const HelpPath = path.join(__dirname, '../public/Help.html')
-// app.use(express.static(HelpPath))
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
-// Home 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Sergio Haddad'
+        name: 'Andrew Mead'
     })
 })
 
-// About
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Sergio'
+        name: 'Andrew Mead'
     })
 })
 
-//Weather
-app.get('/WeatherEx',(req,res) => {
-    res.render('WeatherEx',{
-        Title: 'Welcome to The Weather Page!'
-    })
-})
-
-//Help
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is me sergio',
-        
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
     })
 })
-app.get('/Weather', (req ,res) => {
-    if(!req.query.address){
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
         return res.send({
             error: 'You must provide an address!'
         })
     }
-    geocode(req.query.address, (error, {latitude,longtitude,location}={}) =>{
-        if(error){
-            return res.send({error})
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
         }
-        forecast(latitude,longtitude,(error,forecastData)=>{
-            if(error){
-                return res.send({error})
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
             }
-            res.send({ 
-                forecast:forecastData,
+
+            res.send({
+                forecast: forecastData,
                 location,
-                address:req.query.address
+                address: req.query.address
             })
         })
     })
 })
-//PocketManagment 
-app.get('/PocketManag',(req,res) => {
-    res.render('PocketManag'
-    )
-}) 
-//getingDiscountResult
-// app.get('/Pocky',(req,res) =>{
-//     Dsc(1000)
-// })
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
 app.listen(port, () => {
-    console.log('Server is up on Port 3000.'+ port)
+    console.log('Server is up on port ' + port)
 })
--- src/utils/PocketTest.js
+++
@@ -1,5 +0,0 @@
-const DiscountValue = (a,callback) =>{
-    let Discount=a*0.5;
-    callback(Discount)
-}
-module.exports = DiscountValue;
